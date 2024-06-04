c1b3eeb29f252baf7eed5bae2a002d24b546f9cd
confrontertech <142077542+confronter@users.noreply.github.com>
2024-06-04 11:24:13 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -59,32 +59,32 @@ router.get('/', async (req, res) => {
 				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: '' + b64data });
 	
 				   let SIGMA_MD_TEXT = `
-*𝘒𝘌𝘐𝘛𝘏 𝘚𝘌𝘚𝘚𝘐𝘖𝘕 𝘙𝘜𝘕𝘕𝘐𝘕𝘎*
-*𝘒𝘌𝘐𝘛𝘏 𝘛𝘌𝘊𝘏*
-*𝙊𝙒𝙉𝙀𝙍 : 𝘒𝘌𝘐𝘛𝘏 𝘒𝘌𝘐𝘡𝘡𝘈𝘏*
+𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁 𝚂𝙴𝚂𝚂𝙸𝙾𝙽 𝚁𝚄𝙽𝙽𝙸𝙽𝙶**
+*𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁*
+*𝙊𝙒𝙉𝙀𝙍 : 𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁 𝚃𝙴𝙲𝙷*
 ____________________________________
 ╔════◇
-║『 𝗞𝗘𝗜𝗧𝗛 𝗠𝗗 𝙄𝙎 𝙍𝙀𝘼𝘿𝙔 𝙏𝙊 𝘿𝙀𝙋𝙇𝙊𝙔』
+║『  𝗗𝗥𝗔𝗚𝗢𝗡 𝗠𝗗 𝙄𝙎 𝙍𝙀𝘼𝘿𝙔 𝙏𝙊 𝘿𝙀𝙋𝙇𝙊𝙔』
 ║ OK YOUR SESSION IS READY COPY IT  
 ║ AND HOST IT ON HEROKU.
 ╚════════════════════╝
 ╔═════◇
 ║ 『••• OWNER INFO •••』
-║ ❒ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: _https://www.youtube.com/@keithkeizzah_
+║ ❒ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: _https://www.youtube.com/@Confrontertech_
 
-║ ❒ 𝐎𝐰𝐧𝐞𝐫: _https://wa.me/254748387615_
+║ ❒ 𝐎𝐰𝐧𝐞𝐫: _https://wa.me/254796283064
 
-║ ❒ 𝐑𝐞𝐩𝐨: _https://github.com/keithkeizzah/hunchoV8_
+║ ❒ 𝐑𝐞𝐩𝐨: _https://github.com/confronter/Dragon-Md
 
-║ ❒ 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: _https://chat.whatsapp.com/KeEYrEX8UDdGD4m4GbdHaA_
+║ ❒ 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: _https://chat.whatsapp.com/Cl7CwM1UC9YEOWEiCzLAfe_
 
-║ ❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47_
+║ ❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029Vag3MeuGJP8LZb1Okj39_
 
-║ ❒ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/keizzah_keith_
+║ ❒ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/confronter.__
 
-║ 🐯🐯🐯
+║ 🐲🐉🐲
 ╚════════════════════╝ 
- *©𝗞𝗘𝗜𝗧𝗛 𝗧𝗘𝗖𝗛*
+ *🐉𝚋𝚢 𝙲𝙾𝙽𝙵𝚁𝙾𝙽𝚃𝙴𝚁🐉*
 ___________________________________
 
 Don't Forget To Give Star⭐ To My Repo`
